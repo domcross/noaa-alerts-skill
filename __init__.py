c054c3bd70365383bc66dcf05f21be377ae6fcd7
domcross <dkreutz@t-online.de>
2019-07-10 21:28:45 +0200

    scheduler for automatic alerts

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill  #, intent_file_handler
+from mycroft.skills.core import intent_handler, intent_file_handler
 from mycroft.audio import wait_while_speaking
 from noaa_sdk import noaa
 from datetime import datetime, date
@@ -27,13 +28,46 @@ class NoaaAlerts(MycroftSkill):
         if self.settings.get('auto_alert', False):
             self.monitoring = True
             self.update_interval = self.settings.get("update_interval", 10) * 60
-            self.schedule_repeating_event(handler=self._auto_alert_handler, when=None, frequency=self.update_interval, name='NOAAalerts')
+            self.schedule_repeating_event(handler=self.auto_alert_handler, when=datetime.now(), frequency=self.update_interval, name='NOAAalerts')
             self.log.info("auto_alert on, update_interval {} seconds".format(self.update_interval))
         else:
             self.monitoring = False
             self.cancel_scheduled_event('NOAAalerts')
             self.log.info("auto_alert off")
 
+    #def auto_alert_handler(self, message):
+    def auto_alert_handler(self):
+        self.log.info("auto_alert_handler")
+        self._check_for_alerts(self.zone_id)
+        filteredalerts = []
+        for ap in self.alerts:
+            #ap = alert['properties']
+            status = ap['status']
+            msgType = ap['messageType']
+            urgency = ap['urgency']
+            severity = ap['severity']
+            certainty = ap['certainty']
+            # filter messages: only actual and severity/certainty/urgency according to skill setting
+            if status=='Actual' and msgType in ['Alert'] and severity in "Extreme,Severe" and certainty in "Observed" and urgency in "Immediate":
+                filteredalerts.append(ap)
+
+        self.log.info("found alerts: {}".format(len(filteredalerts)))
+
+        if filteredalerts:
+            self.status = "speaking"
+            self.speak_dialog("alerts.noaa")
+            for alert in filteredalerts:
+                #event = alert["event"]
+                headline = alert['headline']
+                instruction = alert['instruction']
+
+                wait_while_speaking()
+                #if self.status == "speaking":
+                #    self.speak(event)
+                if self.status == "speaking":
+                    self.speak(headline)
+                if self.status == "speaking":
+                    self.speak(instruction) 
 
     @intent_file_handler('alerts.noaa.intent')
     def handle_alerts_noaa(self, message):
@@ -42,7 +76,10 @@ class NoaaAlerts(MycroftSkill):
             self.speak_dialog('error')
             return
 
-        foundalerts = self._check_for_alerts(self.zone_id)
+        if self.monitoring:
+            foundalerts = len(self.alerts) > 0
+        else:
+            foundalerts = self._check_for_alerts(self.zone_id)
         if not foundalerts:
             self.speak_dialog("noalerts.noaa")
             return
@@ -81,45 +118,13 @@ class NoaaAlerts(MycroftSkill):
         else:
             self.speak_dialog("noalerts.noaa")
 
-    def _auto_alert_handler(self):
-        self.log.info("_auto_alert_handler")
-        self._check_for_alerts(self.zone_id)
-        filteredalerts = []
-        for ap in self.alerts:
-            #ap = alert['properties']
-            status = ap['status']
-            msgType = ap['messageType']
-            urgency = ap['urgency']
-            severity = ap['severity']
-            certainty = ap['certainty']
-            # filter messages: only actual and severity/certainty/urgency according to skill setting
-            if status=='Actual' and msgType in ['Alert', 'Update'] and severity in "Extreme,Severe" and certainty in "Observed" and urgency in "Immediate":
-                filteredalerts.append(ap)
-
-        self.log.info("found alerts: {}".format(len(filteredalerts)))
-
-        if filteredalerts:
-            self.status = "speaking"
-            self.speak_dialog("alerts.noaa")
-            for alert in filteredalerts:
-                #event = alert["event"]
-                headline = alert['headline']
-                instruction = alert['instruction']
-
-                wait_while_speaking()
-                #if self.status == "speaking":
-                #    self.speak(event)
-                if self.status == "speaking":
-                    self.speak(headline)
-                if self.status == "speaking":
-                    self.speak(instruction)
 
     def _check_for_alerts(self, zone_id=""):
         foundalerts = False
         if not zone_id:
             self.alerts = []
             return foundalerts
-        self.log.info("_check_for_alerts")
+        self.log.debug("_check_for_alerts")
         newalerts = self.noaa.alerts(zone=zone_id)
 
         for alert in newalerts['features']:
